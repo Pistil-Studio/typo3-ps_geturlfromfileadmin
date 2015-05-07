97af5ccbcab6a99dcce7cf744fcd79ad95f1f412
Pistil-Studio <pf@pistil-studio.fr>
2015-05-07 09:43:06 +0200

    icon alignment correction in backend

--- ps_geturlfromfileadmin/lib/class.tx_manipulateFileListIcon.php
+++ ps_geturlfromfileadmin/lib/class.tx_manipulateFileListIcon.php
@@ -15,7 +15,7 @@ class tx_manipulateFileListIcon implements \TYPO3\CMS\Filelist\FileListEditIconH
 	$domain = (( $_SERVER['HTTPS'] == 'on ') ? 'https://' : 'http://').$_SERVER['SERVER_NAME'];
 	$fullPath = $domain.'/fileadmin/'.$matches[1];
 
-	$img = '<img src="'.$parentObject->backPath.'../typo3conf/ext/ps_geturlfromfileadmin/res/url.png" height="16" width="16" title="Show url" />';
+	$img = '<img src="'.$parentObject->backPath.'../typo3conf/ext/ps_geturlfromfileadmin/res/url.png" style="vertical-align: middle" height="16" width="16" title="Show url" />';
 
 	//$cells['copyPath'] = '<a href="#" onclick="$(this).next().toggle(); return false;">'.$img.'</a><input type="text" value="'.$fullPath.'" size="30" style="margin-left: 3px;display: none"/>';
 	$cells['copyPath'] = '<a href="#" onclick="window.prompt(\'Copy to clipboard: Ctrl+C, Enter\', \''.$fullPath.'\');">'.$img.'</a><input type="text" value="'.$fullPath.'" size="30" style="margin-left: 3px;display: none"/>';
